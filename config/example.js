7497b84bc447deaada733b4bba26f6df0f93c689
gaumeister <derek@apigrate.com>
2023-07-12 10:58:43 -0500

    cleanup unused config dir

--- config/example.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-  auth_file_path: "/Users/Me/Documents/temp/qbo/auth.json",
-  client_id: "abc",//Your quickbooks app id
-  client_secret: "123"//Your quickbooks app id
-};
--- config/index.js
+++
@@ -1,5 +0,0 @@
-/*
-  Typically you need to provide a development.json and production.json file
-  in this directory that contain environment-specifc settings.
-*/
-module.exports = require('./' + (process.env.NODE_ENV || 'development') + '.js');
